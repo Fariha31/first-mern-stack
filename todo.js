b50233ffd8255c52f6766107944e8bd139caecc7
Fariha31 <62641238+Fariha31@users.noreply.github.com>
2020-06-16 14:19:12 -0700

    final

--- todo.js
+++ todo.js
@@ -1,6 +1,30 @@
-var btn = (document.getElementById("todobtn").onclick = function () {
-  var input = document.getElementById("myinput").value;
+window.onload = function () {
+  var btn = document.getElementById("todobtn");
+  btn.onclick = handleBtn;
+  var dbtn = document.getElementById("deletebtn");
+  dbtn.onclick = DeleteBtn;
+};
+var handleBtn = function () {
+  var input1 = document.getElementById("myinput").value;
+  var ulist1 = document.getElementById("ulist");
   var list = document.createElement("li");
-  var todo = document.createTextNode(input.value);
-  list.appendChild(todo);
-});
+  var delbtn = document.createElement("button");
+  var btnvalue = document.createTextNode("Delete Todo");
+  delbtn.appendChild(btnvalue);
+  var att = document.createAttribute("id");
+  att.value = "deletebtn";
+  delbtn.setAttributeNode(att);
+  delbtn.onclick = DeleteBtn;
+  var to = document.createTextNode(input1 + " ");
+  list.appendChild(to);
+  var input2 = (document.getElementById("myinput").value = " ");
+  list.appendChild(delbtn);
+  ulist1.appendChild(list);
+
+  //ulist is already in body ,if not then document.body.append(ulist)
+};
+var DeleteBtn = function (e) {
+  var eve = e.target;
+  var list2 = eve.parentNode;
+  list2.parentNode.removeChild(list2);
+};
